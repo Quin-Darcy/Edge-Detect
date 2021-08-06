603e04dd835683ff7138f08bb764e3462b05e57e
Quin Darcy <75754352+Quin-Darcy@users.noreply.github.com>
2021-08-06 03:42:29 +0000

    Added new image

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from modules import im_proc
 from PIL import Image
 
 def main():
-    I = im_proc.Convolve('Images/Originals/gear.jpg')
-    I.Edge(5)
+    I = im_proc.Convolve('Images/Originals/man.jpg')
+    I.Edge()
 
 if __name__ == '__main__':
     main()
